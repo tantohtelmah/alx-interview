576903f339262e9c8092e67d98ec46a4c7d72de5
tantohtelmah <tantohtelmah@gmail.com>
2024-09-08 22:46:49 -0400

    tasks

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,37 +1,37 @@
 #!/usr/bin/node
 
-const request = require ('request');
+const request = require('request');
 const movieId = process.argv[2];
 
 if (!movieId) {
-    console.error('Please provide a Movie ID as the first argument.');
-    process.exit(1);
+  console.error('Please provide a Movie ID as the first argument.');
+  process.exit(1);
 }
 
 const filmUrl = `https://swapi.dev/api/films/${movieId}/`;
 
 request(filmUrl, { json: true }, (err, res, body) => {
-    if (err) {
+  if (err) {
+    return console.error(err);
+  }
+
+  if (res.statusCode !== 200) {
+    return console.error(`Failed to fetch film data: ${res.statusCode}`);
+  }
+
+  const characterUrls = body.characters;
+
+  characterUrls.forEach((url) => {
+    request(url, { json: true }, (err, res, body) => {
+      if (err) {
         return console.error(err);
-    }
+      }
 
-    if (res.statusCode !== 200) {
-        return console.error(`Failed to fetch film data: ${res.statusCode}`);
-    }
+      if (res.statusCode !== 200) {
+        return console.error(`Failed to fetch character data: ${res.statusCode}`);
+      }
 
-    const characterUrls = body.characters;
-
-    characterUrls.forEach((url) => {
-        request(url, { json: true }, (err, res, body) => {
-            if (err) {
-                return console.error(err);
-            }
-
-            if (res.statusCode !== 200) {
-                return console.error(`Failed to fetch character data: ${res.statusCode}`);
-            }
-
-            console.log(body.name);
-        });
+      console.log(body.name);
     });
+  });
 });
